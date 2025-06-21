4cda2ba4c6115fe6d532a533a0c92d4278ce2163
the-thunder-volt <lunpediya@gmail.com>
2025-06-21 11:09:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,52 +1,74 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <title>Upcoming</title>
-</head>
-<body>
-    <div class="notification">
-        <h3>!!! Site is Under Maintainance You might find some features broken !!!</h3>
-    </div>
-    <div class="content" >
-        working...
-        <!-- <div class="content-cat">
-            <p>web developing</p>
-        </div>
-        <div class="content-cat">
-            discord server developing/manager
-        </div>
-        <div class="content-cat">
-            banner editor
-        </div>
-        <div class="content-cat">
-            <p>video editor</p>
-        </div> -->
+const lines = [
+    "Hey, Nice to see you here!",
+    "You can find more about me just visit the links all over."
+  ];
+  
+  const container = document.getElementById("typewriter");
+  container.innerHTML = '';
+  
+  let lineIndex = 0;
+  
+  function typeLine() {
+    if (lineIndex >= lines.length) return;
+  
+    const lineText = lines[lineIndex];
+    const div = document.createElement("div");
+    div.className = "line";
+    container.appendChild(div);
+  
+    let charIndex = 0;
+  
+    const interval = setInterval(() => {
+      if (charIndex < lineText.length) {
+        div.textContent += lineText[charIndex++];
+      } else {
+        clearInterval(interval);
+        lineIndex++;
+        setTimeout(typeLine, 600); // Delay before next line
+      }
+    }, 60);
+  }
+  
+  typeLine();
+  // window.addEventListener("DOMContentLoaded", () => {
+  //   setTimeout(() => {
+  //     document.getElementById("sideDiv").classList.add("show");
+  //   }, 1000);
+  // });
 
-        <!-- <img src="images\profile.png" alt="">
-        <img src="images\profile.png" alt="">
-        <img src="images\profile.png" alt=""> -->
-    </div>
-    <button></button>
 
-    <div class="main-body">
-        <img src="" alt="">
-        <p></p>
-    </div>
-    <div width="300px"  class="ftr">
-        <footer></footer>
-    </div>
-    <!-- Upcoming
-    <div class="parent">
-        <div class="child">
-            <button>child</button>
-            <div></div>
-        </div>
-        <div class="child2">
-            <div>child2</div>
-        </div>
-    </div> -->
-</body>
-</html>
+// document.addEventListener("DOMContentLoaded", () => {
+//     const aboutText = `Hey, Nice to see you here!
+//     You can find more about me more just visit the links all over.`;
+//     // const aboutText = "I’m a passionate web developer creating modern and responsive websites.";
+//     const element = document.getElementById("typewriter");
+  
+//     let index = 0;
+  
+//     function type(){
+//         if (index < aboutText.length){
+//             element.textContent += aboutText.charAt(index);
+//             index++;
+//             setTimeout(type, 50); // adjust typing speed
+//         }
+        
+        
+//     }
+//     // let index2 = 0;
+    
+//     // function type2()
+//     // {
+//     //     if (index2 < aboutText2.length){
+//     //         element.textContent += aboutText2.charAt(index2);
+//     //         index2++;
+//     //         setTimeout(type2, 50); // adjust typing speed
+//     //     }
+//     //     }
+  
+//     type();
+//     // setTimeout(() => {
+//     //     setInterval(500)
+//     // }, timeout);
+//     // type2();
+//   });
+  
